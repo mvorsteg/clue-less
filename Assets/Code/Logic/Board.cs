df24cc5d54ca46e38e00e6b9dad066e941e2fc07
Matthew Vorsteg <44066626+mvorsteg@users.noreply.github.com>
2023-03-19 11:40:25 -0400

    Adding validation for moving rooms (no hallways yet)

--- Assets/Code/Logic/Board.cs
+++ Assets/Code/Logic/Board.cs
@@ -1,6 +1,66 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    public Room[][] rooms;
+    public RoomType[,] rooms;
+    public Dictionary<RoomType, RoomType> secretPassages;
+
+    private void Awake()
+    {
+        rooms = new RoomType[,]
+        {
+            { RoomType.Study, RoomType.Hall, RoomType.Lounge },
+            { RoomType.Library, RoomType.BilliardRoom, RoomType.DiningRoom },
+            { RoomType.BilliardRoom, RoomType.Ballroom, RoomType.Kitchen} 
+        };
+        secretPassages = new Dictionary<RoomType, RoomType> 
+        {
+            { RoomType.Study, RoomType.Kitchen },
+            { RoomType.Lounge, RoomType.BilliardRoom },
+            { RoomType.BilliardRoom, RoomType.Lounge },
+            { RoomType.Kitchen, RoomType.Study }
+        };
+    }
+
+    public bool IsValidMove(RoomType startRoom, RoomType destRoom)
+    {
+        // get index of start room
+        int row = -1, col = -1;
+        for (int i = 0; i < rooms.GetLength(0); i++)
+        {
+            for (int j = 0; j < rooms.GetLength(1); j++)
+            {
+                if (rooms[i, j] == startRoom)
+                {
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+
+        // check row to left
+        if (row > 0 && rooms[row - 1, col] == destRoom)
+        {
+            return true;
+        }
+        if (row < rooms.GetLength(0) && rooms[row + 1, col] == destRoom)
+        {
+            return true;
+        }
+        if (col > 0 && rooms[row, col - 1] == destRoom)
+        {
+            return true;
+        }
+        if (col < rooms.GetLength(1) && rooms[row, col + 1] == destRoom)
+        {
+            return true;
+        }
+        if (secretPassages.TryGetValue(startRoom, out RoomType secretRoom))
+        {
+            return secretRoom == destRoom;
+        }
+        return false;
+    }
 }
--- Assets/Code/Logic/Room.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class Room : MonoBehaviour
-{
-    public string name;
-
-}
