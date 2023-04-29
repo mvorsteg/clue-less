7fd2bc0d7bc26003be0b8b92d57c53ae4b7dab2d
Matthew Vorsteg <44066626+mvorsteg@users.noreply.github.com>
2023-04-29 01:44:41 -0400

    fixing bug where cluesheet would be empty on game start if not clicked on at least once

--- Assets/Code/Clues/ClueSheet.cs
+++ Assets/Code/Clues/ClueSheet.cs
@@ -7,45 +7,57 @@ public class ClueSheet : MonoBehaviour
 {
     public Transform clueLabelParent, playerLabelParent;
     public GameObject[] columns;
+
+    private bool initialized = false;
     
     private void Start()
     {
-        int i = 0;
-        foreach (CharacterType character in Enum.GetValues(typeof(CharacterType)))
+        if (!initialized)
         {
-            Text text = clueLabelParent.GetChild(i).GetComponent<Text>();
-            text.text = character.ToString();
-            i++;
-        }
-        foreach (WeaponType weapon in Enum.GetValues(typeof(WeaponType)))
-        {
-            Text text = clueLabelParent.GetChild(i).GetComponent<Text>();
-            text.text = weapon.ToString();
-            i++;
-        }
-        foreach (RoomType room in Enum.GetValues(typeof(RoomType)))
-        {
-            Text text = clueLabelParent.GetChild(i).GetComponent<Text>();
-            text.text = room.ToString();
-            i++;
-            if (i >= clueLabelParent.childCount)
+            int i = 0;
+            foreach (CharacterType character in Enum.GetValues(typeof(CharacterType)))
             {
-                break;
+                Text text = clueLabelParent.GetChild(i).GetComponent<Text>();
+                text.text = character.ToString();
+                i++;
             }
-        }
+            foreach (WeaponType weapon in Enum.GetValues(typeof(WeaponType)))
+            {
+                Text text = clueLabelParent.GetChild(i).GetComponent<Text>();
+                text.text = weapon.ToString();
+                i++;
+            }
+            foreach (RoomType room in Enum.GetValues(typeof(RoomType)))
+            {
+                Text text = clueLabelParent.GetChild(i).GetComponent<Text>();
+                text.text = room.ToString();
+                i++;
+                if (i >= clueLabelParent.childCount)
+                {
+                    break;
+                }
+            }
 
-        foreach (GameObject col in columns)
-        {
-            col.SetActive(false);
-        }
-        for (int j = 0; j < playerLabelParent.childCount; j++)
-        {
-            playerLabelParent.GetChild(j).gameObject.SetActive(false);
+            foreach (GameObject col in columns)
+            {
+                col.SetActive(false);
+            }
+            for (int j = 0; j < playerLabelParent.childCount; j++)
+            {
+                playerLabelParent.GetChild(j).gameObject.SetActive(false);
+            }
+
+            initialized = true;
         }
     }
 
     public void StartGame(List<string> otherPlayers)
     {
+        // quick and dirty fix to get around initialization issue (if StartGame called before Start we disable all cols)
+        if (!initialized)
+        {
+            Start();
+        }
         for (int i = 0; i < otherPlayers.Count; i++)
         {
             Text text = playerLabelParent.GetChild(i).GetComponent<Text>();
