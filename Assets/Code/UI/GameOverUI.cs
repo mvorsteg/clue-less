3cfc330fb755714cbee84edbf5073fc7baa268d7
Matthew Vorsteg <44066626+mvorsteg@users.noreply.github.com>
2023-05-03 20:47:49 -0400

    Adding more details to game over modals

--- Assets/Code/UI/GameOverUI.cs
+++ Assets/Code/UI/GameOverUI.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 
 public class GameOverUI : MonoBehaviour
 {
-    public Text text;
+    public Text titleText, bodyText;
     public GameObject displayScreen;
 
     private void Start()
@@ -14,22 +14,37 @@ public class GameOverUI : MonoBehaviour
 
     public void NotifyGameOver(string player, GameOverType type)
     {
-        string plural = "s";
-        if (player.ToLower() == "you")
-        {
-            plural = "";
-        }
+        bool isYou = player.ToLower() == "you";
         if (type == GameOverType.Lose)
         {
-            text.text = String.Format("{0} lose{2}!", player, plural);
+            if (isYou)
+            {
+                titleText.text = String.Format("You Lose");
+                bodyText.text = String.Format("Your guess was incorrect.\nYou are out for the rest of the game.\nBetter luck next time!");
+            }
+            else
+            {
+                titleText.text = String.Format("{0} Loses", player);
+                bodyText.text = String.Format("{0}'s guess was incorrect.\nThey are out for the rest of the game.", player);
+            }
         }
         else if (type == GameOverType.Win)
         {
-            text.text = String.Format("{0} win{2}!", player, plural);
+            if (isYou)
+            {
+                titleText.text = String.Format("You Win");
+                bodyText.text = String.Format("Your guess was correct.\nYou win!.\nThanks for playing!");
+            }
+            else
+            {
+                titleText.text = String.Format("{0} Wins", player);
+                bodyText.text = String.Format("{0}'s guess was correct.\nThey win!\nThanks for playing!", player);
+            }
         }
         else if (type == GameOverType.Error)
         {
-            text.text = String.Format("Unfortunately an error has occurred");
+            titleText.text = String.Format("Error");
+            bodyText.text = String.Format("Unfortunately a communication error has occurred.\nThe game will not be able to continue.\nPlease ensure everyone playing has an internet connection.");
         }
         displayScreen.SetActive(true);
     }
